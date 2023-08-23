8f404861c3c3201e72269dbbfb95eddad53883b7
Tim Stack <tstack43215@yahoo.com>
2023-08-23 16:58:28 -0500

    Initial Commit

---
+++ SchrodingerEquationMatrix/SchrodingerEquationMatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchrodingerEquationMatrixApp.swift
+//  SchrodingerEquationMatrix
+//
+//  Created by IIT PHYS 440 on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SchrodingerEquationMatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
